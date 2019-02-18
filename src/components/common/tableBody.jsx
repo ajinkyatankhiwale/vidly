1214008e30e9e2605f545de0edce5d60f0c91526
Ajinkya Anant Tankhiwale EXT <gn014@OA.PNRAD.NET>
2019-02-18 18:00:40 +0100

    Variable name changed

--- src/components/common/tableBody.jsx
+++ src/components/common/tableBody.jsx
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import _ from "lodash";
 
 class TableBody extends Component {
-  renderCellContent = (dItem, tItem) => {
-    if (tItem.content) return tItem.content(dItem);
-    return _.get(dItem, tItem.key);
+  renderCellContent = (dItem, thItem) => {
+    if (thItem.content) return thItem.content(dItem);
+    return _.get(dItem, thItem.key);
   };
 
   render() {
@@ -14,9 +14,9 @@ class TableBody extends Component {
       <tbody>
         {data.map(dItem => (
           <tr key={dItem._id}>
-            {tableHeaders.map(tItem => (
-              <td key={dItem._id + tItem.key}>
-                {this.renderCellContent(dItem, tItem)}
+            {tableHeaders.map(thItem => (
+              <td key={dItem._id + thItem.key}>
+                {this.renderCellContent(dItem, thItem)}
               </td>
             ))}
           </tr>
